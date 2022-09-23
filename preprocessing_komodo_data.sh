524b5668bcac56b88db2b417b6db56fbf1f10e12
behzadk <behzad.karkaria@gmail.com>
2022-09-23 12:17:39 +0100

    added preprocessing scripts

---
+++ preprocessing_komodo_data.sh
@@ -0,0 +1,5 @@
+cd data/komodo_scraper
+
+export PATH=/rds/user/bk445/hpc-work/prodigal/:$PATH
+
+python -u main.py
---
+++ preprocessing_melanie_data.sh
@@ -0,0 +1,7 @@
+cd data/melanie_data/
+
+# Download melaine genomes
+python -u get_melanie_genomes.py
+
+# Perform preprocessing
+python -u preprocessing.py
